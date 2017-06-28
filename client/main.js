1579e730c1162ed32a356b1929d7cf7c84431509
Tessa <Tessachi33@yahoo.com>
2017-06-27 19:49:11 -0700

    import tasks.js

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import '../imports/ui/body.js';
 import './main.html';
+import '../imports/api/tasks.js';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
